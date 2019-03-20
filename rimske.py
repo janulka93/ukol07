5778ce9166c0a771172897ea8a42ed0d1dfc8053
janulka93 <48135082+janulka93@users.noreply.github.com>
2019-03-20 15:03:19 +0100

    chybi prepocet

--- rimske.py
+++ rimske.py
@@ -1,46 +1,75 @@
 pismena = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 prepis = [1, 5, 10, 50, 100, 500, 1000]
-kombinace = pismena, prepis
-print(kombinace)
-rimske = list(input('Zadej římskou číslici: ').upper())
-for prvek in rimske:
-    if prvek not in pismena:
-        print('Toto není římské písmeno! S tím ti nepomohu.')
-        break
+#kombinace = pismena, prepis
+#print(kombinace)
 
-seznamCisel = []
-for prvek in rimske:
+def kontrolaVstupu(seznam, vstup):
+  '''
+  Vstup a kontrola, zda je římské číslo + převedení na velká písmena.
+  '''
+  for prvek in vstup: 
+    if prvek not in seznam:
+        print('Toto není římské písmeno! S tím ti nepomohu.')
+        return None
+  return vstup
+
+def prirazeni(pismena, prepis, rimske):
+  seznamCisel = []
+  for prvek in rimske:
     umisteni = pismena.index(prvek)
     prirazeni = prepis[umisteni]
     seznamCisel.append(prirazeni)
-print(seznamCisel)
+  return seznamCisel
 
-cislo = 0
-novy_seznam = []
-for prvek in seznamCisel:
-    cislo = prvek
-    if prvek >= cislo:
-        znamenko = '+'
-        novy_seznam.append(prvek)
-        novy_seznam.append(znamenko)
+# Pro zápis dalších přirozených čísel platí tato pravidla:
 
-        cislo = prvek
-    else:
-        znamenko = '-'
-        novy_seznam.append(prvek)
-        novy_seznam.append(znamenko)
-
-        cislo = prvek
-print(novy_seznam)
+# čísla zapsaná stejnými znaky vedle sebe se sčítají
+# jestliže je znak menšího čísla napsán za znakem většího čísla, pak se obě tato čísla sčítají
+# je-li znak většího čísla napsán za znakem menšího čísla, pak od většího se odečítá menší číslo
+# znaky I, X, C se mohou opakovat nejvýše třikrát; znaky V, L, D se v každém čísle vyskytují nejvýše jednou; znak M se může opakovat libovolněkrát 
 
 
+def vypocet(seznam):
+  cislo = 0
+  for i in range(len(seznam)-1):
+    cislo = cislo1
+    cislo1 = int(seznam[i])
+    cislo2 = int(seznam[i+1])
+    if cislo1 < cislo2:
+      cislo = 
+    for j in range (1, len(seznam)):
+      cislo2 = int(seznam[j])
+      if cislo1 < cislo2:
+        cislo1 = cislo1 - cislo2
+        break
+      else:
+        cislo1 = cislo1 + cislo2
+        break
+      break
+  print(cislo1)
+  return cislo
 
 
-
-
-def prevod(rimske):
+def prevod():
     '''Převede římskou číslici na arabskou. (snad)
     '''
+    rimske = list(input('Zadej římskou číslici: ').upper())
+    if kontrolaVstupu(pismena, rimske) != None:
+      cisla = prirazeni(pismena, prepis, rimske)
+      print(cisla)
+      cislo = vypocet(cisla)
+      #print(cislo)
+
+
+
+    #print(rimske)
+
+
+
+prevod()
+
+  
+
 #vstup
 #kontrola, zda je římská, pokud ne - konec
 #pak nějak přidat do seznamu
